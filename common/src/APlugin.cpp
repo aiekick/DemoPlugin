83f5764b9689b0b2abaa0d68f285fecf01cdd535
aiekick <aiekick@funparadigm.com>
2023-06-24 16:09:40 +0200

    -

--- common/src/APlugin.cpp
+++ common/src/APlugin.cpp
@@ -10,8 +10,10 @@ APlugin::~APlugin()
 
 bool APlugin::Init(Singleton* vGuiHelperPtr)
 {
+	// the static instance of the singleton was reinit during the plugin loading
 	Singleton::Instance()->print();
 
+	// so we transfer the singleton instance from the app to the plugin
 	Singleton::Instance(vGuiHelperPtr);
 	Singleton::Instance()->print();
 
